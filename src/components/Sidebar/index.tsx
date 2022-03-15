ba7d99fe20d86736842ecd1d2436379f33d22e86
Gustavo Gava <gustavogava123@hotmail.com>
2022-03-15 19:48:29 -0300

    feat: divided Pagination into components

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,5 +1,6 @@
 import { Box, Stack } from "@chakra-ui/react";
 import { RiContactsLine, RiDashboardLine, RiGitMergeLine, RiInputMethodLine } from "react-icons/ri";
+
 import { NavLink } from "./NavLink";
 import { NavSection } from "./NavSection";
 
