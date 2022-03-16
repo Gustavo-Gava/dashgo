8f38a4deb568eba39f42ee8ad6bcf033d96df15d
Gustavo Gava <gustavogava123@hotmail.com>
2022-03-16 15:07:57 -0300

    feat: show actived link on sidebar

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,4 +1,5 @@
 import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, VStack } from "@chakra-ui/react";
+import Link from "next/link";
 import { Input } from "../../components/Form/Input";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
@@ -22,7 +23,7 @@ export default function CreateUser() {
                 <Input name="email" type="email" label="E-mail" />
               </SimpleGrid>
 
-              <SimpleGrid  minChildWidth="240px" spacing={["6", "8"]} w="100%">
+              <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
                 <Input name="password" type="password" label="Senha" />
                 <Input name="password_confirmation" type="password" label="Confirmação da senha" />
               </SimpleGrid>
@@ -30,7 +31,9 @@ export default function CreateUser() {
 
             <Flex mt="8" justify="flex-end">
               <HStack spacing="4">
-                <Button colorScheme="whiteAlpha">Cancelar</Button>
+                <Link href="/users" passHref>
+                  <Button colorScheme="whiteAlpha">Cancelar</Button>
+                </Link>
                 <Button colorScheme="pink">Salvar</Button>
               </HStack>
             </Flex>
