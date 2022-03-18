ecc81baa1aba48c8a513deba6c99275ce819d4fd
Gustavo Gava <gustavogava123@hotmail.com>
2022-03-18 13:04:04 -0300

    feat: added initial configuration for miragejs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { AppProps } from 'next/app'
+
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext'
+import { makeServer } from '../services/mirage'
 
 import { theme } from '../styles/theme'
+
+if (process.env.NODE_ENV === 'development') {
+  makeServer()
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
